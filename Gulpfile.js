02a74aab0a6c621186f2c69e6eb903d5b94a7661
Luke Boga Mitchell <luke.bogamitchell@gmail.com>
2017-02-01 14:09:17 +0000

    Gulpfile Midleman base

---
+++ Gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+
+// PostCSS
+var postcss = require('gulp-postcss');
+var reporter = require('postcss-reporter');
+var scss = require('postcss-scss');
+var stylelint = require('stylelint');
+
+/*
+** CSS
+**************************************************/
+
+gulp.task('css', function() {
+  return gulp.src('./source/css/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./.tmp'));
+});
+
+gulp.task('lint:css', function() {
+  return gulp.src('./source/css/**/*.scss')
+    .pipe(postcss(
+      [
+        stylelint(),
+        reporter({ clearMessages: true })
+      ], {
+        syntax: scss
+      }
+    ));
+});
+
+/*
+** Server
+**************************************************/
+
+gulp.task('watch', [ 'css' ], function(gulpCallback) {
+	browserSync.init({
+		proxy: 'http://localhost:4567', /* Middleman server */
+		open: false,
+		reloadDelay: 100, /* Concurrency fix */
+		reloadDebounce: 500, /* Concurrency fix */
+		// reloadOnRestart: true,
+		files: [
+      './.tmp/main.css',
+      './source/**/*.slim'
+    ],
+    /* BrowserSync proxy */
+		port: 7000,
+    /* BrowserSync UI */
+		ui: { port: 7001 }
+	}
+  /**
+   * Server running.
+   * Begin watching files...
+   */
+  ,function callback() {
+		gulp.watch('./source/css/**/*.(sass|scss)', [ 'css' ]);
+
+		gulpCallback();
+	});
+});
+
+/*
+** Task-flows
+**************************************************/
+
+gulp.task('default', function(callback) {
+	runSequence('css', [ 'watch' ], callback);
+});
+
+gulp.task('build', function(callback) {
+	runSequence('css', callback);
+});
+
+gulp.task('lint', function(callback) {
+	runSequence('lint:css', callback);
+});
