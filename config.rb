89533aa2120bb8fd1f18fdc2eaccdc587fed1384
Luke Boga Mitchell <luke.bogamitchell@gmail.com>
2017-02-02 11:54:21 +0000

    Dir rejig

--- config.rb
+++ config.rb
@@ -1,48 +1,44 @@
 require 'slim'
 
-###
-# Page options, layouts, aliases and proxies
-###
+##############################
+## Page options
+##############################
 
-# Per-page layout changes:
-#
-# With no layout
+## Layouts
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+## Directories
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+## Proxy pages
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# General configuration
+##############################
+## Helpers
+##############################
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
 # helpers do
-#   def some_helper
-#     "Helping"
-#   end
+#
 # end
 
-# **************************************************
-# Build
-# **************************************************
+##############################
+## Build
+##############################
 
 # MM build
 configure :build do
   ignore 'css/**'
   ignore 'js/**'
+  ignore 'layouts/**'
   ignore 'partials/**'
 end
 
-# External pipeline
+##############################
+## External pipeline
+##############################
+
 activate :external_pipeline,
   :name => 'gulp',
   :command => (build? ? 'npm run build' : 'npm start'),
