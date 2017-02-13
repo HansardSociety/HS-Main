4ca5be9f797da6500dd56b5e4d05bd6a0247be30
Luke Boga Mitchell <luke.bogamitchell@gmail.com>
2017-02-13 13:41:18 +0000

    Change data name to hs and don't ignore

--- config.rb
+++ config.rb
@@ -46,7 +46,7 @@ end
 ##############################
 
 activate :contentful do |f|
-  f.space         = { contentful: 'xkbace0jm9pp' }
+  f.space         = { hs: 'xkbace0jm9pp' }
   f.access_token  = '94f4e91e316abf614c2a6537891640e9ab4c80a74b1b482accc0f1f22eefa688'
   f.cda_query     = { limit: 1000 }
   f.content_types = {
