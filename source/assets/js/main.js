ba5d42787d76a1e12c0a08b86edae5694faec260
Luke Boga Mitchell <luke.bogamitchell@gmail.com>
2017-04-05 14:03:26 +0100

    Exclusive buttons/ state - DONE

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -5,7 +5,6 @@ import { throttle } from 'lodash/fp';
 //  Global Variables
 ////////////////////////////////////////////////////////////
 
-var globalState = document.querySelector('.js-global-state');
 var navbar = document.querySelector('.navbar');
 
 ////////////////////////////////////////////////////////////
@@ -13,20 +12,24 @@ var navbar = document.querySelector('.navbar');
 ////////////////////////////////////////////////////////////
 
 // Toggle class
-function toggleClass(obj, state) {
-  obj.classList.toggle(state);
+function toggleClass(obj, className) {
+  obj.classList.toggle(className);
 }
 
 // Toggle state
-function toggleState(obj, elem, target) {
-  var state = obj.getAttribute('data-change-state');
+function toggleState(trigger, className, target) {
+  var state = trigger.getAttribute('data-change-state');
 
-  toggleClass(obj, elem);
-  target.classList.toggle(state);
+  // Toggle trigger class
+  toggleClass(trigger, className);
+
+  // Toggle target scope state
+  toggleClass(target, state);
+
 }
 
 // For-each loop
-var forEach = function (array, cb, scope) {
+var forEach = function(array, cb, scope) {
   for (var i = 0; i < array.length; i++) {
     cb.call(scope, i, array[i]);
   }
@@ -36,39 +39,47 @@ var forEach = function (array, cb, scope) {
 //  Buttons
 ////////////////////////////////////////////////////////////
 
+var globalState = document.querySelector('.js-global-state');
+
+// If exclusive event
+function exclusiveState(trigger) {
+  var exclusiveTriggers = document.querySelectorAll('.js-trigger-exclusive');
+
+  // If an exclusive event...
+  if (trigger.classList.contains('js-trigger-exclusive')) {
+
+    // Loop through all exclusive triggers
+    forEach(exclusiveTriggers, function(index, elem) {
+
+      // If (this) trigger element != other exclusive triggers...
+      if ((elem != trigger) && (elem.classList.contains('js-on'))) {
+
+        // Toggle global or local state depending on elem...
+        elem.classList.toString().indexOf('global') > -1 ? toggleState(elem, 'js-on', globalState) : toggleState(elem, 'js-on', elem.closest('.js-local-state'))
+      }
+    });
+  }
+}
+
 // Button change state
 var buttonsGlobal = document.querySelectorAll('.button.js-trigger-global');
 var buttonsLocal = document.querySelectorAll('.button.js-trigger-local');
 
-// Global
-forEach(buttonsGlobal, function (index, value) {
-  var exclusiveTriggers = document.querySelectorAll('.js-trigger-exclusive');
+// Global state change
+forEach(buttonsGlobal, function (index, button) {
 
-  value.onclick = function() {
+  button.onclick = function() {
     var trigger = this;
 
     // Toggle global state
     toggleState(trigger, 'js-on', globalState);
 
-    // If an exclusive event...
-    if (trigger.classList.contains('js-trigger-exclusive')) {
-
-      // Loop through all exclusive triggers
-      forEach(exclusiveTriggers, function(index, elem) {
-
-        // If (this) trigger element != other exclusive triggers...
-        if ((elem != trigger) && (elem.classList.contains('js-on'))) {
-          var state = elem.getAttribute('data-change-state')
-
-          // Remove on- and global-states
-          toggleState(elem, 'js-on', globalState);
-        }
-      });
-    }
+    // If exclusive event
+    exclusiveState(trigger);
 
     // If menu is activated, shrink navbar...
-    var menuDesktopOn = globalState.classList.toString().indexOf('js-show-menu-desktop') > -1
-    var navbarOn = navbar.classList.toString().indexOf('js-on') > -1
+    var menuDesktopOn = globalState.classList.toString().indexOf('js-show-menu-desktop') > -1;
+    var navbarOn = navbar.classList.toString().indexOf('js-on') > -1;
 
     if (menuDesktopOn === true && navbarOn === false) {
       navbar.classList.toggle('js-on');
@@ -79,12 +90,16 @@ forEach(buttonsGlobal, function (index, value) {
   }
 });
 
-// Local
+// Local state change
 forEach(buttonsLocal, function (index, value) {
-  value.onclick = function() {
-    var local = this.closest('.js-local-state');
 
-    toggleState(this, 'js-on', local);
+  value.onclick = function() {
+    var localState = this.closest('.js-local-state');
+
+    toggleState(this, 'js-on', localState);
+
+    // If exclusive event
+    exclusiveState(this);
   }
 });
 
@@ -95,9 +110,9 @@ forEach(buttonsLocal, function (index, value) {
 // Navbar shrink
 window.addEventListener('scroll', _.throttle(function() {
 
-  if ( window.pageYOffset >= 1 ) {
+  if (window.pageYOffset >= 1) {
     navbar.classList.add('js-on');
   } else {
     navbar.classList.remove('js-on');
   }
-}, 600));
+}, 500));
