829528f2ddcf32a43caba2131c0579316c8bd31c
Luke Boga Mitchell <luke.bogamitchell@gmail.com>
2017-08-14 12:01:07 +0100

    Banner heights JS (complete)

--- source/assets/js/modules/misc.js
+++ source/assets/js/modules/misc.js
@@ -1,13 +1,21 @@
 import smoothScroll from 'smooth-scroll';
 
-////////////////////////////////////////////////////////////
+// Banner height
+const bannerHeight = (() => {
+  const vhFull = Math.max(window.innerHeight);
+  const vhSemi = Math.max(window.innerHeight * .6666);
+  const bannerFull = document.querySelector('.banner--full');
+  const bannerSemi = document.querySelector('.banner--semi');
+
+  if (bannerFull) bannerFull.style.height = `${ vhFull }px`;
+  if (bannerSemi) bannerSemi.style.height = `${ vhSemi }px`;
+})();
+
 //  Smooth scroll
 //  https://github.com/cferdinandi/smooth-scroll
-////////////////////////////////////////////////////////////
-
 const bannerScroll = new smoothScroll('.banner__scroll', {
   offset: 64,
   speed: 600
 });
 
-export { bannerScroll }
+export { bannerScroll, bannerHeight }
