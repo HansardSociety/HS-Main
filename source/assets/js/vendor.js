bf2a715837c72a3aaafe0d6338af43d3bd1c7aa7
Luke Boga Mitchell <luke.bogamitchell@gmail.com>
2017-11-16 17:27:45 +0000

    IE11 (complete)

--- source/assets/js/vendor.js
+++ source/assets/js/vendor.js
@@ -1,11 +1,19 @@
-// Polyfills
+/*		=Polyfills
+  ========================================================================== */
+
+// IE11
 import "core-js/fn/symbol/iterator"
+import "core-js/fn/array/from"
+import "core-js/fn/array/find-index"
 import "core-js/es6/symbol"
+
 import "promise-polyfill"
 import "picturefill"
 import "whatwg-fetch"
 
-// Libs
+/*		=Libs
+  ========================================================================== */
+
 import "lodash/throttle"
 import "smooth-scroll"
 import "flickity"
