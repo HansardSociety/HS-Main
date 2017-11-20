16354d7b068669c60fe19206a082707c25b53d47
Luke Boga Mitchell <luke.bogamitchell@gmail.com>
2017-11-20 17:18:04 +0000

    Rev fix 6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,19 +4,18 @@ import gulp from "gulp"
 import runSequence from "run-sequence"
 
 // Streams
-import { customAppCSS, customVendorCSS, vendorCSS } from "./lib/gulp/styles"
+import { cssApp, cssVendor } from "./lib/gulp/styles"
 import { iconSprite } from "./lib/gulp/images"
 import { js } from "./lib/gulp/scripts"
 import { devServer } from "./lib/gulp/dev-server"
 
 // Core tasks
-gulp.task("css:custom-app", () => customAppCSS())
-gulp.task("css:custom-vendor", () => customVendorCSS())
-gulp.task("css:vendor", () => vendorCSS())
+gulp.task("css:app", () => cssApp())
+gulp.task("css:vendor", () => cssVendor())
 gulp.task("js", () => js())
 gulp.task("sprite", () => iconSprite())
 
 // Builds.
 gulp.task("default", () => runSequence(["watch"]))
-gulp.task("watch", ["css:custom-app", "css:custom-vendor", "css:vendor", "js"], () => devServer())
-gulp.task("prod", () => runSequence(["css:custom-app", "css:custom-vendor", "css:vendor", "js"]))
+gulp.task("watch", ["css:app", "css:vendor", "js"], () => devServer())
+gulp.task("prod", () => runSequence(["css:app", "css:vendor", "js"]))
