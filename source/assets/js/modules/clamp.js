5bf50058f304728c151702be095b180420b6ffab
Luke Boga Mitchell <luke.bogamitchell@gmail.com>
2017-12-09 13:44:33 +0000

    Add line-clamp to side-cards
    * Installed and initiated line-clamp
    * Changed side-card content positioning to work with line-clamp

---
+++ source/assets/js/modules/clamp.js
@@ -0,0 +1,18 @@
+import lineClamp from "line-clamp"
+
+const clamp = (() => {
+  const clamp2 = document.querySelectorAll(".JS-clamp-2")
+  const clamp3 = document.querySelectorAll(".JS-clamp-3")
+
+  for (let i of clamp2) {
+
+    lineClamp(i, { lineCount: 4 })
+  }
+
+  for (let i of clamp3) {
+
+    lineClamp(i, { lineCount: 5 })
+  }
+})()
+
+export { clamp }
