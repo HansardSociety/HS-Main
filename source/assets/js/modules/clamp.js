6a073eefc6dccb9e7059506fd101f36d09b462aa
LukeBM <luke.bogamitchell@gmail.com>
2017-12-12 15:56:01 +0000

    Fix shavejs 2

--- source/assets/js/modules/clamp.js
+++ source/assets/js/modules/clamp.js
@@ -4,8 +4,8 @@ const clamp = (() => {
   const clamp2 = document.querySelectorAll(".JS-clamp-2")
   const clamp3 = document.querySelectorAll(".JS-clamp-3")
 
-  for (let i of clamp2) { shave(i, 51) }
-  for (let i of clamp3) { shave(i, 78) }
+  for (let i of clamp2) { shave(i, 52) }
+  for (let i of clamp3) { shave(i, 79) }
 })()
 
 export { clamp }
