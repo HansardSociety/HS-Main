01bbbc51a08e4dd1ec3f243a81ddc8b833493c36
LukeBM <luke.bogamitchell@gmail.com>
2017-12-12 21:52:07 +0000

    De-refine clamp px

--- source/assets/js/modules/clamp.js
+++ source/assets/js/modules/clamp.js
@@ -5,11 +5,11 @@ const clamp = (() => {
   const clamp3 = document.querySelectorAll(".JS-clamp-3")
 
   for (let i of clamp2) {
-    shave(i, 53.6, { spaces: false })
+    shave(i, 54, { spaces: false })
   }
 
   for (let i of clamp3) {
-    shave(i, 76.8, { spaces: false })
+    shave(i, 81, { spaces: false })
   }
 })()
 
