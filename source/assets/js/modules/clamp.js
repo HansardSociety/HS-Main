8f637dba831359362e8f65e1972d0fb3a4724ece
LukeBM <luke.bogamitchell@gmail.com>
2017-12-12 16:52:35 +0000

    Test shave t2

--- source/assets/js/modules/clamp.js
+++ source/assets/js/modules/clamp.js
@@ -5,7 +5,7 @@ const clamp = (() => {
   const clamp3 = document.querySelectorAll(".JS-clamp-3")
 
   for (let i of clamp2) { shave(i, 54) }
-  for (let i of clamp3) { shave(i, 81) }
+  for (let i of clamp3) { shave(i, 78) }
 })()
 
 export { clamp }
