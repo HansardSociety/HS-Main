bb3cd158d4342a1dac0c6698c1f5d5c9a8e275e4
LukeBM <luke.bogamitchell@gmail.com>
2018-02-08 17:33:22 +0000

    Testing BT dropin

--- source/assets/js/checkout.js
+++ source/assets/js/checkout.js
@@ -4,7 +4,12 @@ const button = document.querySelector('#dropin-submit-button');
 
 dropin.create({
   authorization: "sandbox_99kwdtcc_gqg8v5627nfqvf2y",
-  container: "#dropin-container"
+  container: "#dropin-container",
+  card: {
+    overrides: {
+      // fields:
+    }
+  }
 }, function(createErr, instance) {
   button.addEventListener("click"), function() {
     instance.requestPaymentMethod(function(requestPaymentMethodErr, payload) {
