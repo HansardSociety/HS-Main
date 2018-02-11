7ee8c937c4ac67420531895b7a6ae825912ced5a
LukeBM <luke.bogamitchell@gmail.com>
2018-02-11 18:43:16 +0000

    Braintree wip

--- source/assets/js/checkout.js
+++ source/assets/js/checkout.js
@@ -1,19 +1,82 @@
-import dropin from "braintree-web"
+import braintree from "braintree-web"
 
-const button = document.querySelector('#dropin-submit-button');
+var form = document.querySelector("#payment-form");
+var submit = form.querySelector("button[type='submit']");
 
-dropin.create({
-  authorization: "sandbox_99kwdtcc_gqg8v5627nfqvf2y",
-  container: "#dropin-container",
-  card: {
-    overrides: {
-      // fields:
-    }
+var checkoutStyles = {
+  "input": {
+    "font-size": "14px"
+  },
+  "input.invalid": {
+    "color": "red"
+  },
+  "input.valid": {
+    "color": "green"
   }
-}, function(createErr, instance) {
-  button.addEventListener("click"), function() {
-    instance.requestPaymentMethod(function(requestPaymentMethodErr, payload) {
-      // Submit payload.nonce to server...
+}
+
+var paymentFields = {
+  number: {
+    selector: "#card-number",
+    placeholder: "41111 1111 1111 1111"
+  },
+  cvv: {
+    selector: "#cvv",
+      placeholder: "123"
+  },
+  expirationDate: {
+    selector: "#expiration-date",
+      placeholder: "10 / 2019"
+  }
+}
+
+/*		=Set up client
+  ========================================================================== */
+
+braintree.client.create({
+  authorization: "sandbox_99kwdtcc_gqg8v5627nfqvf2y"
+}, clientDidCreate)
+
+/*		=Client instance
+  ========================================================================== */
+
+function clientDidCreate(clientErr, clientInstance) {
+
+  if (clientErr) {
+    console.error(clientErr);
+    return;
+  }
+
+  braintree.hostedFields.create({
+    client: clientInstance,
+    styles: checkoutStyles,
+    fields: paymentFields
+  }, hostedFieldsDidCreate)
+}
+
+/*		=Hosted fields instance
+  ========================================================================== */
+
+function hostedFieldsDidCreate(hostedErr, hostedInstance) {
+
+  if (hostedErr) {
+    console.error(hostedErr);
+    return;
+  }
+
+  submit.removeAttribute("disabled")
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    hostedInstance.tokenize(function(tokenizeErr, payload) {
+
+      if (tokenizeErr) {
+        console.error(tokenizeErr);
+        return;
+      }
+
+      console.log("Nonce: " + payload.nonce)
     })
-  }
-})
+  }, false)
+}
