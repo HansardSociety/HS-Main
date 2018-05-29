4ad84edcf9f7b7a76b7ad25c723a3274d8bd3385
LukeBM <luke.bogamitchell@gmail.com>
2018-05-29 17:41:17 +0100

    Banner - wip

--- source/assets/js/modules/carousel.js
+++ source/assets/js/modules/carousel.js
@@ -3,49 +3,53 @@ import Flickity from "flickity"
 /*		=Options
   ========================================================================== */
 
-var flktyOpts = {
+var flickityOpts = {
   cellSelector: ".carousel__item",
   cellAlign: "left",
   contain: true,
-  friction: .8,
   wrapAround: true,
   initialIndex: 0,
   lazyLoad: false,
   pageDots: true,
   prevNextButtons: false,
-  selectedAttraction: .2,
   setGallerySize: false
 }
 
 /*		=Panel carousel
   ========================================================================== */
 
-const panelCarousel = (() => {
+const panelCarousel = (function() {
   const allCarouselContainers = document.querySelectorAll(".carousel")
 
   for (let carouselContainer of allCarouselContainers) {
     const carousel = carouselContainer.querySelector(".carousel__inner")
+    const carouselHome = carouselContainer.querySelector(".carousel__inner.JS-carousel--home")
     const actions = carouselContainer.querySelector(".carousel__actions")
     const prevBtn = actions.querySelector(".carousel__prev")
     const nextBtn = actions.querySelector(".carousel__next")
-
-    if (carouselContainer.querySelector(".banner__image")) {
-      flktyOpts = Object.assign({
-        autoPlay: 5000,
-        pauseAutoPlayOnHover: false
-      }, flktyOpts)
-    }
 
     /**
      * Initiate Flickity on window load to prevent iOS
      * setting height of item too early.
      */
     window.addEventListener("load", function() {
-      const flkty = new Flickity(carousel, flktyOpts)
+
+      if (carouselHome) {
+        var flickityOptsHome = Object.assign({}, flickityOpts, {
+          autoPlay: 5500,
+          pauseAutoPlayOnHover: false
+        })
+        var flickity = new Flickity(carouselHome, flickityOptsHome)
+
+      } else {
+        var flickityOptsCards = Object.assign({}, flickityOpts, {
+        })
+        var flickity = new Flickity(carousel, flickityOptsCards)
+      }
+
       const allItems = carousel.querySelectorAll(".carousel__item")
 
       var maxHeight = 0
-
       for (let item of allItems) {
         var thisHeight = item.clientHeight
 
@@ -55,8 +59,14 @@ const panelCarousel = (() => {
       carousel.classList.add("JS-loaded")
       carousel.style.height = `${ maxHeight + 4 }px`
 
-      prevBtn.addEventListener("click", () => flkty.previous())
-      nextBtn.addEventListener("click", () => flkty.next())
+      prevBtn.addEventListener("click", () => {
+        flickity.previous()
+        flickity.pausePlayer()
+      })
+      nextBtn.addEventListener("click", () => {
+        flickity.next()
+        flickity.pausePlayer()
+      })
     })
   }
 })()
