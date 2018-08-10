fe944773841302e0ff2daebee01b3cffd802b4e3
LukeBM <luke.bogamitchell@gmail.com>
2018-08-10 13:17:29 +0100

    Newsletter

--- lib/ruby/netlify.rb
+++ lib/ruby/netlify.rb
@@ -6,7 +6,7 @@ module Netlify
       file << "/home /\n"
       file << "/audit /research/audit-of-political-engagement\n"
       file << "/blog/bridging-representative-and-direct-democracy-ireland's-citizens'-assemblies /blog/bridging-representative-and-direct-democracy-irelands-citizens-assemblies\n"
-      file << "/newsletter https://www.hansardsociety.org.uk/#panel-0\n"
+      file << "/newsletter https://www.hansardsociety.org.uk/about/newsletter\n"
       file << "/resources/publications/* /publications/:splat\n"
       file << "/* /about/404.html 404\n"
     end
