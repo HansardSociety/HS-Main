3eb9f4d71091e84c69bd86c6cee7a6b422c27d2b
LukeBM <luke.bogamitchell@gmail.com>
2018-09-17 17:30:15 +0100

    Add SortBy

--- source/assets/js/modules/search.js
+++ source/assets/js/modules/search.js
@@ -19,17 +19,26 @@ const algoliaSearch = (() =>
     const search = instantSearch({
       appId: "AJC8ZDIWBJ",
       apiKey: "66a9759f27ae50a3c41abf7b82181a11",
-      indexName: "Main_Search_DB",
+      indexName: "Main_Search_DB_By_Date",
       routing: true,
       searchParameters: {
         hitsPerPage: 6
       },
-      attributesForFaceting: ["category"]
+      attributesForFaceting: ["category", "sub_category", "theme", "sub_theme"]
     })
 
-    // console.log(config["categories"])
-
     for (let block of searchBlocks) {
+
+      var searchFilters = ""
+
+      if (!block.dataset.searchCategory) {
+        searchFilters = `theme:'${block.dataset.searchTheme}'`
+      }
+
+      search.searchParameters.filters = searchFilters;
+
+      /*  =Core
+       *****************************************/
 
       // Search widget
       search.addWidget(
@@ -49,17 +58,21 @@ const algoliaSearch = (() =>
           container: block.querySelector(".search__results"),
           showMoreLabel: "Load more…",
           templates: {
-            empty: "No results",
+            empty: "...",
             item: template("main-card")
           },
         }),
       )
+
+      /*  =Refinement
+       *****************************************/
 
       // Refinement widget 1
       search.addWidget(
         instantSearch.widgets.refinementList({
           container: block.querySelector(".search__filters > .search__filter-1"),
           attributeName: "sub_theme",
+          sortBy: ["name:asc"],
           operator: "or",
           templates: {
             header: "<span>Sub-theme:</span>"
@@ -72,6 +85,7 @@ const algoliaSearch = (() =>
         instantSearch.widgets.refinementList({
           container: block.querySelector(".search__filters > .search__filter-2"),
           attributeName: "category",
+          sortBy: ["name:asc"],
           operator: "or",
           templates: {
             header: "<span>Category:</span>"
@@ -79,17 +93,32 @@ const algoliaSearch = (() =>
         })
       )
 
+      // Sort
+      search.addWidget(
+        instantSearch.widgets.sortBySelector({
+          container: block.querySelector(".search__sort-select"),
+          indices: [
+            { name: "Main_Search_DB_By_Date", label: "Date (desc)" },
+            { name: "Main_Search_DB", label: "Relevance" }
+          ]
+        })
+      )
+
+      // Clear
       search.addWidget(
         instantSearch.widgets.clearAll({
-          container: block.querySelector(".search__clear"),
+          container: block.querySelector(".search__clear-inner"),
+          clearsQuery: true,
           templates: {
-            link: "<span class='search__clear-link'>Clear search<span>"
+            link: "<span class='search__clear-link'>Reset all<span>"
           }
         })
       )
 
-      // Lazy load images
-      search.on("render", function () {
+      /*  =Laxy load
+       *****************************************/
+
+      search.on("render", function (q) {
         const cards = block.querySelectorAll(".main-card")
 
         for (let card of cards) {
@@ -102,8 +131,6 @@ const algoliaSearch = (() =>
           }
         }
       })
-
-
 
       search.start()
     }
