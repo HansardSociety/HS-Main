6324c64c79562dc06f3433686564b4651006c521
LukeBM <luke.bogamitchell@gmail.com>
2018-10-16 15:04:18 +0100

    Switch banner to px

--- source/assets/js/modules/banner.js
+++ source/assets/js/modules/banner.js
@@ -7,22 +7,22 @@ const bannerHeight = (() => {
   const bannerHome = document.querySelectorAll(".JS-banner--full .carousel__item")
 
   if (matchMedia("screen and (min-width: 600px)").matches) {
-    var vhLg = Math.ceil((windowHeight * .9) / 18)
-    var vhMd = Math.ceil((windowHeight * .9) / 18)
+    var vhLg = Math.ceil(windowHeight * .9)
+    var vhMd = Math.ceil(windowHeight * .9)
 
   } else {
-    var vhLg = Math.ceil(windowHeight / 18)
-    var vhMd = Math.ceil(windowHeight / 18)
+    var vhLg = Math.ceil(windowHeight)
+    var vhMd = Math.ceil(windowHeight)
   }
 
 
-  if (bannerFull) bannerFull.style.height = `${ vhLg }rem`
-  if (bannerSemi) bannerSemi.style.height = `${ vhLg }rem`
+  if (bannerFull) bannerFull.style.height = `${ vhLg }px`
+  if (bannerSemi) bannerSemi.style.height = `${ vhLg }px`
 
   // Home banner
   if (bannerHome) {
     for (let carouselItem of bannerHome) {
-      carouselItem.style.height = `${ vhLg }rem`
+      carouselItem.style.height = `${ vhLg }px`
     }
   }
 
