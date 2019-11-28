49f80c75f457850901e9d7dc8af57ff52cd7568a
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-11-28 17:49:49 +0000

    History — WIP

--- source/assets/js/modules/history.js
+++ source/assets/js/modules/history.js
@@ -1,6 +1,20 @@
 
-const history = (() => {
+const historyNav = (() => {
+  let title = document.querySelector(".banner__title").innerText;
+  let url = window.location.pathname;
 
+  let state = Object.assign(
+    {},
+    history.state,
+    {
+      title,
+      url
+    }
+  );
+
+  history.pushState(state, title, url);
+
+  console.log(history.state)
 })();
 
-export { history }
+export { historyNav }
