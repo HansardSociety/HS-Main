32c19b01b86b9bde33cedc7c3ed41cc22d370bff
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-11-29 16:46:05 +0000

    History — WIP

--- source/assets/js/modules/history.js
+++ source/assets/js/modules/history.js
@@ -1,20 +1,34 @@
 
 const historyNav = (() => {
+  let titleKey = "PAGE-TITLE";
+  let pathKey = "PAGE-PATH";
+
   let title = document.querySelector(".banner__title").innerText;
-  let url = window.location.pathname;
+  let path = window.location.pathname;
+  let hasPageStorage = sessionStorage.getItem(`1-${ titleKey }`);
 
-  let state = Object.assign(
-    {},
-    history.state,
-    {
-      title,
-      url
+
+  if (!hasPageStorage) {
+    sessionStorage.setItem(`1-${ titleKey }`, title);
+    sessionStorage.setItem(`1-${ pathKey }`, path);
+
+  } else {
+    const pageNo = (sessionStorage.length / 2) + 1;
+    let storedPagePaths = [];
+
+    Object.keys(sessionStorage).map(key => {
+      if (key.indexOf(pathKey) > -1) {
+        storedPagePaths.push(sessionStorage[key]);
+      }
+    });
+
+    if (!storedPagePaths.includes(path)) {
+      sessionStorage.setItem(`${ pageNo }-${ titleKey }`, title);
+      sessionStorage.setItem(`${ pageNo }-${ pathKey }`, path);
     }
-  );
+  }
 
-  history.pushState(state, title, url);
-
-  console.log(history.state)
+  console.log(sessionStorage)
 })();
 
 export { historyNav }
