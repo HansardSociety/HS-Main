5e3671e96b560d4d353fbf39cdc3b47f14a9da17
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-12-11 17:57:14 +0000

    Plyr - WIP

--- source/assets/js/modules/video.js
+++ source/assets/js/modules/video.js
@@ -1,11 +1,13 @@
 import Plyr from "plyr";
 
-const player = (() => {
+const video = (() => {
   const videoElem = document.querySelectorAll(".plyr__video-embed");
 
   if (videoElem) {
-    videoElem.forEach((vid) => new Plyr(vid))
+    videoElem.forEach((vid) => {
+      const player = new Plyr(vid);
+    })
   }
 })()
 
-export { player }
+export { video }
