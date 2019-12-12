20329317254e8d301be9f8195ca904ce6e45086d
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-12-12 21:17:54 +0000

    Guides – Polish v2

--- source/assets/js/modules/video.js
+++ source/assets/js/modules/video.js
@@ -1,7 +1,16 @@
 import Plyr from "plyr";
 
 const video = (() => {
-  const players = Plyr.setup(".video");
+  Array.from(document.querySelectorAll(".video")).map((p) => {
+    const player = new Plyr(p);
+
+    if (p.dataset) {
+      const startTime = Number(p.dataset.start);
+      const endTime = Number(p.dataset.start);
+
+      player.seek(startTime);
+    }
+  })
 })()
 
 export { video }
