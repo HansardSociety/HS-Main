7688d347b2a86075c4398fc52d81cb61b51f85ef
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-12-12 22:33:56 +0000

    Guides – Polish v3

--- source/assets/js/modules/video.js
+++ source/assets/js/modules/video.js
@@ -4,12 +4,14 @@ const video = (() => {
   Array.from(document.querySelectorAll(".video")).map((p) => {
     const player = new Plyr(p);
 
-    if (p.dataset) {
-      const startTime = Number(p.dataset.start);
-      const endTime = Number(p.dataset.start);
+    // if (p.dataset) {
+    //   const startTime = Number(p.dataset.start);
+    //   const endTime = Number(p.dataset.start);
 
-      player.seek(startTime);
-    }
+    //   player.on("ready", (e) => {
+
+    //   })
+    // }
   })
 })()
 
