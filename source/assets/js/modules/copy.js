6a64a3ed0b87c0d6daa97938cbff7eb5c870f88a
Luke Boga Mitchell <lbogamitchell@gmail.com>
2020-03-17 14:48:09 +0000

    Fix modal z-index and toc links

--- source/assets/js/modules/copy.js
+++ source/assets/js/modules/copy.js
@@ -1,40 +1,21 @@
-// const imgCaptions = (() => {
-//   let copyElems = document.querySelectorAll(".copy--lp");
-
-//   if (copyElems) {
-//     copyElems.forEach((copyElem) => {
-//       let imgElems = copyElem.querySelectorAll("img");
-
-//       if (imgElems) {
-//         imgElems.forEach((imgElem) => {
-//           let alt = imgElem.getAttribute("alt");
-//           let caption = document.createElement("span");
-
-//           caption.innerHTML = alt;
-//           caption.classList.add("img-caption")
-
-//           imgElem.parentElement.appendChild(caption);
-//         });
-//       }
-//     });
-//   }
-//   return;
-// })();
-
 const setIcons = (() => {
   let tocLinks = document.querySelectorAll(".toc-link");
 
   if (tocLinks) {
-    let iconTemplate = document.querySelector("script[id='icon-template']").innerHTML.replace(/`/g, "");
+    let iconTemplate = document.querySelector("script[id='icon-template']");
 
-    tocLinks.forEach((el) => {
-      let tocText = el.innerText;
-      let tocIcon = iconTemplate
-        .replace("{{icon}}", "ios-arrow-thin-up")
-        .replace("{{size}}", "xs");
+    if (iconTemplate) {
+      iconTemplate = iconTemplate.innerHTML.replace(/`/g, "");
 
-      el.innerHTML = tocIcon.trim() + tocText.trim();
-    });
+      tocLinks.forEach((el) => {
+        let tocText = el.innerText;
+        let tocIcon = iconTemplate
+          .replace("{{icon}}", "ios-arrow-thin-up")
+          .replace("{{size}}", "xs");
+
+        el.innerHTML = tocIcon.trim() + tocText.trim();
+      });
+    }
   }
   return;
 })();
